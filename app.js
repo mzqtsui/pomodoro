6d1be53ff9c7df1e6c6e232d2c2c4cba7a4c9eed
Matthew Tsui <matthew.zq.tsui@gmail.com>
2016-10-12 20:20:56 -0400

    basic timer running

--- app.js
+++ app.js
@@ -9,35 +9,68 @@
     var stateSpan = document.querySelector(".state");
     stateSpan.innerHTML = stateIcons["play"];
 
-    var timeData = document.querySelector(".session-time");
+    var timeData = document.querySelector(".session-time"),
+        clock = document.querySelector(".clock");
+
+
+    function formatTime(secs) {
+        var hours = Math.floor(secs/60),
+            mins = secs % 60;
+        return  ((hours < 10) ? "0" + hours : hours) + ":" +
+                ((mins < 10) ? "0" + mins : mins);
+    }
+
+    function updateTime(time) {
+        timeData.textContent = formatTime(time);
+    }
+
 
     var timer = (function() {
-        var state = "reset",    // current timer state
-            maxTime = 5 * 60 + 1,  // max time in seconds
-            currTime = maxTime; // current time in seconds
+        const STATES = {
+            "RESET":    0,
+            "RUNNING":  1,
+            "PAUSED":   2
+        };
+
+        var state = STATES.RESET,    // current timer state
+            maxTime = 1,        // max time in seconds
+            currTime = maxTime, // current time in seconds
+            interval;
 
         return {
-            play: () => {},
+            start: () => {
+                state = STATES.RUNNING;
+                interval = setInterval(function() {
+                        currTime -= 1;
+                        if (currTime < 0) {
+                            currTime = 0;
+                            state = STATES.RESET;
+                            clearInterval(interval);
+                        }
+                        updateTime(currTime);
+                    }, 1000);
+
+            },
             reset: () => {
-                state = "reset";
+                state = STATES.RESET;
                 currTime = maxTime;
             },
-            pause: () => {},
+            pause: () => {
+                state = STATES.PAUSED;
+            },
             getTime: () => currTime,
             setTime: (newTime) => {
                 maxTime = newTime;
                 reset();
-            },
-            getFormattedTime: () => {
-                var hours = Math.floor(currTime/60),
-                    mins = currTime % 60;
-                return  ((hours < 10) ? "0" + hours : hours) + ":" +
-                        ((mins < 10) ? "0" + mins : mins);
             }
         };
     }());
 
 
-    timeData.textContent = timer.getFormattedTime();
+    updateTime(timer.getTime());
+
+    clock.addEventListener("click", () => {
+        timer.start();
+    });
 
 })();
