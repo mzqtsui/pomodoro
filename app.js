915654e70621315f4fa31bd4a7f391e045eaef4c
Matthew Tsui <matthew.zq.tsui@gmail.com>
2016-10-12 21:04:05 -0400

    automatically switch work/rest phases

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 (function(){
     "use strict";
 
-    var stateIcons = {
-        "play": "<i class='fa fa-play' aria-hidden='true'></i>",
-        "pause": "<i class='fa fa-pause' aria-hidden='true'></i>"
+    const STATES = {
+        "RESET":    0,
+        "RUNNING":  1,
+        "PAUSED":   2
     };
 
-    var stateSpan = document.querySelector(".state");
-    stateSpan.innerHTML = stateIcons["play"];
+    const STATE_ICONS = {
+        "PLAY": "<i class='fa fa-play' aria-hidden='true'></i>",
+        "PAUSE": "<i class='fa fa-pause' aria-hidden='true'></i>"
+    };
 
-    var timeData = document.querySelector(".session-time"),
-        clock = document.querySelector(".clock");
+    var stateSpan = document.querySelector(".state"),
+        timeData = document.querySelector(".session-time"),
+        clock = document.querySelector(".clock"),
+        sessionName = document.querySelector(".session-name");
 
 
     function formatTime(secs) {
@@ -20,32 +25,55 @@
                 ((mins < 10) ? "0" + mins : mins);
     }
 
+    function updateSessionName(lastSession) {
+        sessionName.textContent = lastSession;
+    }
+
     function updateTime(time) {
         timeData.textContent = formatTime(time);
     }
 
+    function updateHoverIcon(state) {
+        switch (state) {
+            case STATES.RESET:
+                stateSpan.innerHTML = STATE_ICONS.PLAY;
+                break;
+
+            case STATES.RUNNING:
+                stateSpan.innerHTML = STATE_ICONS.PAUSE;
+                break;
+
+            case STATES.PAUSED:
+                stateSpan.innerHTML = STATE_ICONS.PLAY;
+                break;
+            default:
+
+        }
+    }
+
 
     var timer = (function() {
-        const STATES = {
-            "RESET":    0,
-            "RUNNING":  1,
-            "PAUSED":   2
-        };
 
-        var state = STATES.RESET,    // current timer state
-            maxTime = 1,        // max time in seconds
-            currTime = maxTime, // current time in seconds
+        var session = "Work",       // current work/rest state
+            state = STATES.RESET,   // current timer state
+            maxWorkTime = 5,        // max work time in seconds
+            maxRestTime = 3,        // max rest time in seconds
+            currTime = maxWorkTime,     // current time in seconds
             interval;
 
         return {
             start: () => {
                 state = STATES.RUNNING;
+                updateHoverIcon(state);
                 interval = setInterval(function() {
                         currTime -= 1;
                         if (currTime < 0) {
-                            currTime = 0;
-                            state = STATES.RESET;
-                            clearInterval(interval);
+                            session = (session === "Work") ? "Rest" : "Work";
+                            currTime = (session === "Work") ? maxWorkTime : maxRestTime;
+                            clock.classList.toggle("rest");
+                            updateHoverIcon(state);
+                            updateTime(currTime);
+                            updateSessionName(session);
                         }
                         updateTime(currTime);
                     }, 1000);
@@ -53,24 +81,42 @@
             },
             reset: () => {
                 state = STATES.RESET;
-                currTime = maxTime;
+                session = "Work";
+                currTime = maxWorkTime;
+                clock.classList.remove("rest");
+                updateHoverIcon(state);
+                updateTime(currTime);
+                updateSessionName(session);
             },
             pause: () => {
                 state = STATES.PAUSED;
+                updateHoverIcon(state);
+                clearInterval(interval);
             },
             getTime: () => currTime,
-            setTime: (newTime) => {
-                maxTime = newTime;
-                reset();
-            }
+            getState: () => state
         };
     }());
 
-
+    updateHoverIcon(timer.getState());
     updateTime(timer.getTime());
 
     clock.addEventListener("click", () => {
-        timer.start();
+        switch (timer.getState()) {
+            case STATES.RESET:
+                timer.start();
+                break;
+
+            case STATES.RUNNING:
+                timer.pause();
+                break;
+
+            case STATES.PAUSED:
+                timer.start();
+                break;
+            default:
+
+        }
     });
 
 })();
